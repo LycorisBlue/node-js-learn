dbbe31a88fb209a9bd4e1e89f2901cf6f10a6792
LycorisBlue <lostvayneatful@gmail.com>
2024-01-16 00:35:10 +0000

    synchronisation

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 const express = require("express");
-let pokemons = require('./data')
+const morgan = require('morgan');
+const bp = require('body-parser');
+const { Sequelize, DataTypes } = require('sequelize');
+let pokemons = require('./data');
+const PokemonModel = require('./models/pokemon');
+const { success } = require("./help");
 
 const app = express();
 const port = 3000;
 
+const sequelize = new Sequelize(
+    'pokedex',
+    'root',
+    'root',
+    {
+        host: 'localhost',
+        dialect: 'mysql'
+    }
+);
+
+sequelize.authenticate().then(() =>console.log('Connected...')).catch(err => console.error(err));
+
+const Pokemon = PokemonModel(sequelize, DataTypes);
+sequelize.sync({force: true}).then(() =>{console.log("La base de donnée 'Pokedex' a bien été synchronisée")});
+
+const logger = (req, res, next) =>{
+    console.log(`URL: ${req.url}`);
+    next();
+}
+
+app.use(morgan('dev')).use(bp.json());
+
+
+
 app.get('/', (req, res) => res.send("Hello again, Express "));
-
-app.get('/api/pokemons/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const pokemon = pokemons.find(p  => p.id === id);
-    res.send(`Vous avez démandé le pokemon ${pokemon.name}`)
-});
-
 app.get('/api/pokemons', (req, res) => {
-    res.send(`Vous avez ${pokemons.length} pokemons`);
+    const message = "Success"
+    res.json(success(message, pokemons));
 });
 
 app.listen(port, () => console.log(`Notre aplication Node est lancé sur le port: ${port}`));
